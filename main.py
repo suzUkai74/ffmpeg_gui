0bc2f567ee98cfa3b2c3374dade409e135c80e8e
Ryosuke Watanabe <suzukai74@gmail.com>
2024-10-23 11:48:53 +0900

    Add remove audio and video info

--- main.py
+++ main.py
@@ -2,19 +2,42 @@ import flet as ft
 import subprocess
 import os
 import unicodedata
+import cv2
+import math
 
 def main(page: ft.Page):
     page.title = "ffmpeg GUI"
     page.scroll = ft.ScrollMode.AUTO
     PREVIEW_INDEX = 8
+    capture = ""
+
+    def remove_video_info():
+        selected_file.value = ""
+        aspect_text.value = ""
+        definition_text.value = ""
+        video_size_text.value = ""
 
     def pick_files_result(e: ft.FilePickerResultEvent):
-        if len(e.files) == 1:
+        if e.files is None:
+            remove_video_info()
+        elif len(e.files) == 1:
             selected_file.value = e.files[0].path
+            capture = cv2.VideoCapture(selected_file.value)
+            w = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+            h = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            gcd = math.gcd(w, h)
+            aw = int(w / gcd)
+            ah = int(h / gcd)
+            definition_text.value = f"{w}×{h}"
+            aspect_text.value = f"{aw}:{ah}"
+            video_size_text.value = content_size(selected_file.value)
         else:
-            selected_file.value = ""
+            remove_video_info()
 
         selected_file.update()
+        aspect_text.update()
+        definition_text.update()
+        video_size_text.update()
 
     def get_directry_result(e: ft.FilePickerResultEvent):
         selected_directry.value = e.path if e.path else "キャンセルされました。"
@@ -73,18 +96,19 @@ def main(page: ft.Page):
         if not output_file_name_input.value:
             errors.append("保存動画名を指定してください。")
 
-
         if (scale_height_input.value or scale_width_input.value):
             scale_height_input.value = normalize_num(scale_height_input.value)
             scale_width_input.value = normalize_num(scale_width_input.value)
 
-            if (scale_height_input.value and not(scale_width_input.value)) or \
+            if remove_audio.value:
+                errors.append("解像度と音声削除は同時に選択できません。")
+            elif (scale_height_input.value and not(scale_width_input.value)) or \
                 (scale_width_input.value and not(scale_height_input.value)) or \
                 (scale_width_input.value == "-1" and scale_height_input.value == "-1"):
-                errors.append("scaleを正しく設定してください。")
+                errors.append("解像度を正しく設定してください。")
             elif (not scale_height_input.value.isdecimal() and scale_height_input.value != "-1") or \
                 (not scale_width_input.value.isdecimal() and scale_width_input.value != "-1"):
-                errors.append("scaleには数値を設定してください。")
+                errors.append("解像度には数値を設定してください。")
 
         if errors:
             result_text.value = "入力項目が正しくありません。下記内容を確認してください。"
@@ -122,6 +146,12 @@ def main(page: ft.Page):
                     "-vf",
                     f"scale={scale_width_input.value}:{scale_height_input.value}",
                 ]
+            elif remove_audio.value:
+                cmds += [
+                    "-vcodec",
+                    "copy",
+                    "-an",
+                ]
             
             cmds.append(f"{escape_for_zsh(selected_directry.value)}/{output_file_name_input.value}.mp4")
             cp = subprocess.run(' '.join(cmds), shell=True, executable='/bin/zsh', capture_output=True)
@@ -138,6 +168,16 @@ def main(page: ft.Page):
 
     def label_text(text):
         return ft.Text(f"{text}：", width=150)
+
+    def remove_audio_changed(e):
+        scale_height_input.value = ""
+        scale_width_input.value = ""
+        scale_height_input.update()
+        scale_width_input.update()
+
+    def scale_input_changed(e):
+        remove_audio.value = False
+        remove_audio.update()
     
     pick_file = ft.FilePicker(on_result=pick_files_result)
     selected_file = ft.Text()
@@ -161,10 +201,15 @@ def main(page: ft.Page):
     crf_input_slider = ft.Slider(min=0, max=51, divisions=51, value=23, label="{value}", on_change=update_crf_value_text)
     crf_value_text = ft.Text(crf_input_slider.value, width="30")
     crf_help_text = ft.Text("※数値が小さいほど高品質になります。")
-    scale_width_input = ft.TextField(label="Width", width="80")
-    scale_height_input = ft.TextField(label="Height", width="80")
+    aspect_text = ft.Text()
+    definition_text = ft.Text()
+    video_size_text = ft.Text()
+    scale_width_input = ft.TextField(label="Width", width="80", on_change=scale_input_changed)
+    scale_height_input = ft.TextField(label="Height", width="80", on_change=scale_input_changed)
     scale_help_text = ft.Text("※-1を設定するとアスペクト比を維持して自動調整します。")
     result_text = ft.Text()
+    remove_audio = ft.Checkbox(value=False, on_change=remove_audio_changed)
+    remove_audio_help_text = ft.Text("※音声削除と解像度は同時選択できません。")
     page.overlay.extend([pick_file,get_directry])
 
     page.add(
@@ -173,6 +218,24 @@ def main(page: ft.Page):
                 label_text("動画"),
                 video_input_button,
                 selected_file,
+            ]
+        ),
+        ft.Row(
+            [
+                label_text("動画サイズ"),
+                video_size_text,
+            ]
+        ),
+        ft.Row(
+            [
+                label_text("解像度"),
+                definition_text,
+            ]
+        ),
+        ft.Row(
+            [
+                label_text("アスペクト比"),
+                aspect_text,
             ]
         ),
         ft.Row(
@@ -190,7 +253,7 @@ def main(page: ft.Page):
         ),
         ft.Row(
             [
-                label_text("crf"),
+                label_text("品質設定"),
                 crf_input_slider,
                 crf_value_text,
                 crf_help_text,
@@ -198,11 +261,18 @@ def main(page: ft.Page):
         ),
         ft.Row(
             [
-                label_text("scale"),
+                label_text("解像度設定"),
                 scale_width_input,
                 ft.Text("："),
                 scale_height_input,
                 scale_help_text,
+            ]
+        ),
+        ft.Row(
+            [
+                label_text("音声削除"),
+                remove_audio,
+                remove_audio_help_text,
             ]
         ),
         ft.Row(
